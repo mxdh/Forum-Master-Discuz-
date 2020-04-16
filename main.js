7e623c5a2456b4da1342b5eca9f9a18ead90f789
mxdh <20399194+mxdh@users.noreply.github.com>
2020-04-16 08:22:58 +0800

    0.5.2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @name:zh-CN   论坛大师・Discuz!
 // @name:zh-TW   論壇大師・Discuz!
 // @namespace    Forum Master・Discuz!-mxdh
-// @version      0.5.1
+// @version      0.5.2
 // @icon         https://www.discuz.net/favicon.ico
 // @description  Forum Master - Discuz!　Beautify the interface, Remove ads, Enhance functions.
 // @description:en    Forum Master - Discuz!　Beautify the interface, Remove ads, Enhance functions.
@@ -651,7 +651,7 @@
             case 'HOSTLOC':
                 return '󠀠'.repeat(10);
             default:
-                '\n\n[img=1,1]https://img.alicdn.com/dot.gif[/img]'
+                return '\n\n[img=1,1]https://img.alicdn.com/dot.gif[/img]';
         }
     }
 
